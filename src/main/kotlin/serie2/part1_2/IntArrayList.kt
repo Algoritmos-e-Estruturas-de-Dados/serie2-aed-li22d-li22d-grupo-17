7c40400cc72cf7c3760c4873748fef6d22cd73e6
Tomás Antunes <tpantunes11@gmail.com>
2025-04-30 10:14:20 +0100

    2

--- src/main/kotlin/serie2/part1_2/IntArrayList.kt
+++ src/main/kotlin/serie2/part1_2/IntArrayList.kt
@@ -1,12 +1,26 @@
 package serie2.part1_2
 
-class IntArrayList : Iterable <Int> {
+class IntArrayList(val capacity:Int) : Iterable <Int> {
+    val data = IntArray(capacity)
+    var head = 0
+    var size = 0
+    var tail = 0
+
 
     fun append(x:Int):Boolean {
+       if(size==capacity)return false //Se a lista tiver cheia
+        data[tail] = x
+        tail = (tail + 1) % capacity
+        size++
+        return true
+
         TODO("Not yet implemented")
     }
 
     fun get(n:Int):Int?  {
+       if(n>=size||n<0)return null //Indice inválido
+        val index = (head + n) % capacity
+            return data[index]
         TODO("Not yet implemented")
     }
 
